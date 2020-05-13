2ed0f1a874581ec9d89de699d41f2222cea42c54
ahnjeongyoon <puppy4732@gmail.com>
2020-05-13 20:40:25 +0900

    첫번째 커밋

---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    {{ todoItems }}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      todoItems: []
+    }
+  },
+  methods: {
+    fetchTodoItems: function (){
+      //var arr = [];
+      for(var i = 0; localStorage.length; i++){
+        var value = localStorage.key(i);
+        //arr.push(value);
+        this.todoItems.push(value);
+      }
+      //return arr;
+     }
+  },
+  created: function(){
+    this.fetchTodoItems();
+
+    // localStorage.getItem();
+    // axios.get();
+    // $.ajax()
+  }
+
+}
+</script>
+
+<style>
+
+</style>
